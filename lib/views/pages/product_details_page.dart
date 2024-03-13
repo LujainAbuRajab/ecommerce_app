99d31edbd7690e5648d6af33cf452fcc7c837c32
LujainAbuRajab <lujainaburajab@gmail.com>
2024-03-13 20:36:05 +0200

    Product Details Page - Done

--- lib/views/pages/product_details_page.dart
+++ lib/views/pages/product_details_page.dart
@@ -1,3 +1,4 @@
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_app/models/product_item_model.dart';
 import 'package:ecommerce_app/utils/app_colors.dart';
 import 'package:flutter/material.dart';
@@ -34,119 +35,167 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
               icon: const Icon(Icons.favorite_border_rounded)),
         ],
       ),
-      body: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            padding: EdgeInsets.only(top: size.height * 0.08),
-            decoration: BoxDecoration(
-              color: AppColors.grey2,
-            ),
-            child: Image.asset(
-              product.imgUrl,
-              fit: BoxFit.contain,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: size.height * 0.5),
-            child: Container(
+      body: SafeArea(
+        top: false,
+        child: Stack(
+          children: [
+            Container(
               width: double.infinity,
-              decoration: const BoxDecoration(
-                color: AppColors.white,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
+              height: size.height * 0.6,
+              // padding: EdgeInsets.only(top: size.height * 0.09),
+              decoration: BoxDecoration(
+                color: AppColors.grey2,
               ),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              product.name,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .titleLarge!
-                                  .copyWith(
-                                    fontWeight: FontWeight.bold,
+              child: Image.asset(
+                product.imgUrl,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: size.height * 0.5),
+              child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: AppColors.white,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                product.name,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleLarge!
+                                    .copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Icons.star,
+                                    color: AppColors.orange,
                                   ),
-                            ),
-                            Row(
-                              children: [
-                                const Icon(
-                                  Icons.star,
-                                  color: AppColors.orange,
-                                ),
-                                const SizedBox(width: 6),
-                                Text(
-                                  product.averageRate.toString(),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge!
-                                      .copyWith(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        CounterWidget(),
-                      ],
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Size',
-                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                            fontWeight: FontWeight.bold,
+                                  const SizedBox(width: 6),
+                                  Text(
+                                    product.averageRate.toString(),
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .labelLarge!
+                                        .copyWith(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
-                    ),
-                    const SizedBox(height: 8),
-                    Row(
-                      children: ProductSize.values
-                          .map(
-                            (size) => Padding(
-                              padding: const EdgeInsetsDirectional.only(end: 8),
-                              child: InkWell(
-                                onTap: () {
-                                  setState(() {
-                                    selectedSize = size;
-                                  });
-                                },
-                                child: CircleAvatar(
-                                  radius: 23,
-                                  backgroundColor: selectedSize == size
-                                      ? Theme.of(context).primaryColor
-                                      : AppColors.grey2,
-                                  child: Center(
-                                    child: Text(
-                                      size.name,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .labelMedium!
-                                          .copyWith(
-                                            fontWeight: FontWeight.bold,
-                                            color: selectedSize == size
-                                                ? AppColors.white
-                                                : null,
-                                          ),
+                          CounterWidget(),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        'Size',
+                        style:
+                            Theme.of(context).textTheme.titleMedium!.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                      ),
+                      const SizedBox(height: 8),
+                      Row(
+                        children: ProductSize.values
+                            .map(
+                              (size) => Padding(
+                                padding:
+                                    const EdgeInsetsDirectional.only(end: 8),
+                                child: InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      selectedSize = size;
+                                    });
+                                  },
+                                  child: CircleAvatar(
+                                    radius: 23,
+                                    backgroundColor: selectedSize == size
+                                        ? Theme.of(context).primaryColor
+                                        : AppColors.grey2,
+                                    child: Center(
+                                      child: Text(
+                                        size.name,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelMedium!
+                                            .copyWith(
+                                              fontWeight: FontWeight.bold,
+                                              color: selectedSize == size
+                                                  ? AppColors.white
+                                                  : null,
+                                            ),
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
+                            )
+                            .toList(),
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        'Details',
+                        style:
+                            Theme.of(context).textTheme.titleMedium!.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        product.description,
+                        style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                              color: AppColors.grey,
                             ),
-                          )
-                          .toList(),
-                    ),
-                  ],
+                      ),
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '\$${product.price}',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineSmall!
+                                .copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.primary,
+                                ),
+                          ),
+                          SizedBox(
+                            height: 50,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: AppColors.primary,
+                                foregroundColor: AppColors.white,
+                              ),
+                              child: const Text('Add to Order'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
